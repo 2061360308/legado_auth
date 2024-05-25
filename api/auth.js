dcf128dfe420861ebf1df51230a07e5335387e96
2061360308 <2061360308@qq.com>
2024-05-25 18:44:24 +0800

    first commit

---
+++ api/auth.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.get('/api/auth', async (req, res) => {
+  const code = req.query.code;
+  const client_id = process.env.GITHUB_CLIENT_ID;
+  const client_secret = process.env.GITHUB_CLIENT_SECRET;
+  const redirect_uri = process.env.REDIRECT_URI;
+
+  try {
+    const response = await axios.post('https://github.com/login/oauth/access_token', {
+      client_id,
+      client_secret,
+      code,
+      redirect_uri
+    }, {
+      headers: {
+        accept: 'application/json'
+      }
+    });
+
+    const access_token = response.data.access_token;
+
+    // Redirect back to your frontend app with the access token in the URL
+    res.redirect(`http://your-frontend-app.com/#/auth?access_token=${access_token}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('An error occurred while trying to authenticate');
+  }
+});
+
+module.exports = app;
